99629fd28d04730bf15b4cf2a62b3e9ccb32e7ba
vishalpoonacha <vishalpoonacha007@gmail.com>
2022-07-21 20:04:53 +0530

    created basketScreen

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import RestaurantScreen from "./screens/RestaurantScreen";
 import { Provider } from "react-redux";
 import store from "./store";
 import BasketScreen from "./screens/BasketScreen";
+import PreparingOrderScreen from "./screens/PreparingOrderScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -23,6 +24,11 @@ export default function App() {
               name="Basket"
               component={BasketScreen}
               options={{ presentation: "modal", headerShown: false }}
+            />
+            <Stack.Screen
+              name="PreparingOrderScreen"
+              component={PreparingOrderScreen}
+              options={{ presentation: "fullScreenModal", headerShown: false }}
             />
           </Stack.Navigator>
         </TailwindProvider>
---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const PreparingOrderScreen = () => {
+  return (
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center"></SafeAreaView>
+  );
+};
+
+export default PreparingOrderScreen;
